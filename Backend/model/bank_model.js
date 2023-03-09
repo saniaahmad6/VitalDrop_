e3eb554d43fef939489a1a45061b858ef37375cd
Fayez Siddiqui <fayez.siddiqui31@gmail.com>
2023-03-08 23:32:07 -0800

    added bank model and refrenced on tables

--- Backend/model/bank_model.js
+++ Backend/model/bank_model.js
@@ -1,34 +1,30 @@
-//database initialization and connection
-const mysql = require('mysql');
-const mysqlInfo = {
-  user: 'root',
-  host: 'localhost',
-  password: 'password',
-  database: 'VitalDropDB'
+const Bank={
+selectBank: `
+SELECT BloodBank.center_id, AdminUsers.name AS manager,
+    DonationCenters.address, BloodBank.units_available, COUNT(BloodBank.units_available) AS size
+    FROM DonationCenters LEFT JOIN BloodBank 
+    ON Bloodbank.center_id = DonationCenters.id
+    RIGHT JOIN AdminUsers ON AdminUsers.assigned_center = DonationCenters.id
+    GROUP BY bank_id
+` ,   
+selectTotalDonatedBlood: `
+    SELECT COUNT(*) as total_donated_blood FROM BloodBank
+  `,
+
+  selectAvailableBloodTypes: `
+  SELECT BloodBank.blood_type, COUNT(BloodBank.units_available) AS num_of_blood
+  FROM BloodBank 
+  GROUP BY blood_type
+  ORDER BY num_of_blood DESC
+`,
+
 }
 
-var con = mysql.createConnection({ host: mysqlInfo.host, user: mysqlInfo.user, password: mysqlInfo.password });
-con.connect(function (err) {
-  if (err) {
-    console.log("ERROR IN CONNECTING TO MYSQL");
-    throw err;
-  };
-  console.log("Connected!");
-});
-
-const basicQueryCallback = (err, result) => {
-  if (err) throw err;
-  console.log("Query Success!")
-}
-
-con.query(`CREATE DATABASE IF NOT EXISTS ${mysqlInfo.database}`, basicQueryCallback)
-con.changeUser({database : mysqlInfo.database}, function(err) {
-  if (err) throw err;
-});
+module.exports=Bank
 
 
-const table_creations = [
-  "create table if not exists Users(id int PRIMARY KEY, email_id varchar(255) not null, name varchar(255), password varchar(255), UNIQUE(email_id))",
+/*
+ "create table if not exists Users(id int PRIMARY KEY, email_id varchar(255) not null, name varchar(255), password varchar(255), UNIQUE(email_id))",
 
   "create table if not exists DonationCenters(id int PRIMARY KEY, pincode varchar(10) not null, state varchar(255) not null, address varchar(255) not null)",
   
@@ -41,21 +37,6 @@ const table_creations = [
   "create table if not exists Donations(id int PRIMARY KEY, user_id int not null, appointment_id int not null, status varchar(255), FOREIGN KEY(user_id) REFERENCES Users(id), FOREIGN KEY(appointment_id) REFERENCES Appointments(id))",
 
   "create table if not exists Requests(id int PRIMARY KEY, user_id int not null, center_id int not null, blood_type char(4) not null, amount int not null, status varchar(255), FOREIGN KEY(user_id) REFERENCES Users(id), FOREIGN KEY(center_id) REFERENCES DonationCenters(id))"
-]
 
-table_creations.forEach(query => {
-  con.query(query, basicQueryCallback);
-});
 
-//Api starts here
-const express = require('express')
-const app = express()
-const port = 5000
-
-app.get('/', (req, res) => {
-  res.send('Hello World! - VitalDrop')
-})
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+*/
