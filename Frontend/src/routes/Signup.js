d56641d7b6deb4cf28f8684aad51332475a1f006
Sania Ahmad <sania.ahmad483@gmail.com>
2023-03-12 01:22:40 +0530

    create user form added

--- Frontend/src/routes/Signup.js
+++ Frontend/src/routes/Signup.js
@@ -1,12 +1,13 @@
 import Navbar from "../components/Navbar"
 import Footer from "../components/Footer"
 import Heroproj from "../components/Heroproj";
+import UserSignUp from "../components/CreateUser/UserSignUp";
 
 function Signup(){
     return <>
         <Navbar/>
-        <Heroproj heading = "" text=""/>
-        
+        <Heroproj heading = "Join us." text="Let's make a difference"/>
+        <UserSignUp/>
         <Footer/>
     </>
 
